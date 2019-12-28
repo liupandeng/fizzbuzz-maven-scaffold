e21ba72d9b9a04be61a09812dfda6565425e910f
liupandeng <woaixiaowei888>
2019-12-28 13:00:26 +0800

    pass Java-刘攀登-成都项目建设处

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,15 +1,13 @@
 public class FizzBuzz {
     public static String of(int i) {
-        if (i % 5 == 0) {
-            return "Buzz";
-        }
-        if (i % 3 == 0) {
-            return "Fizz";
-        }
         if (i % 3 == 0 && i % 5 == 0) {
             return "FizzBuzz";
+        } else if (i % 3 == 0) {
+            return "Fizz";
+        } else if (i % 5 == 0) {
+            return "Buzz";
         } else {
-            return String.format("%d", i);
+            return i+"";
         }
     }
 
